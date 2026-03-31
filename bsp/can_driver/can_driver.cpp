097bbc7d605137d8387b0a7f1b3808818dffa43f
syhanjin <syhanjin666@gmail.com>
2026-03-31 19:32:35 +0800

    fix(can): 修复有可能情况下多条消息进一次中断没有完全处理的问题

--- bsp/can_driver/can_driver.cpp
+++ bsp/can_driver/can_driver.cpp
@@ -168,17 +168,20 @@ void CAN_RegisterCallback(CAN_HandleTypeDef* hcan, const CAN_FifoReceiveCallback
  */
 void CAN_Fifo0ReceiveCallback(CAN_HandleTypeDef* hcan)
 {
-    CAN_RxHeaderTypeDef header;
-    uint8_t             data[8];
-    if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &header, data) != HAL_OK)
+    while (HAL_CAN_GetRxFifoFillLevel(hcan, CAN_RX_FIFO0) > 0)
     {
-        Error_Handler();
-        return;
+        CAN_RxHeaderTypeDef header;
+        uint8_t             data[8];
+        if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &header, data) != HAL_OK)
+        {
+            Error_Handler();
+            return;
+        }
+        const CAN_CallbackMap* map = get_map(hcan);
+        if (map != nullptr)
+            for (size_t i = 0; i < map->callback_count; i++)
+                map->callbacks[i](hcan, &header, data);
     }
-    const CAN_CallbackMap* map = get_map(hcan);
-    if (map != nullptr)
-        for (size_t i = 0; i < map->callback_count; i++)
-            map->callbacks[i](hcan, &header, data);
 }
 /**
  * CAN Fifo1 接收处理函数
@@ -188,17 +191,20 @@ void CAN_Fifo0ReceiveCallback(CAN_HandleTypeDef* hcan)
  */
 void CAN_Fifo1ReceiveCallback(CAN_HandleTypeDef* hcan)
 {
-    CAN_RxHeaderTypeDef header;
-    uint8_t             data[8];
-    if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO1, &header, data) != HAL_OK)
+    while (HAL_CAN_GetRxFifoFillLevel(hcan, CAN_RX_FIFO0) > 0)
     {
-        Error_Handler();
-        return;
+        CAN_RxHeaderTypeDef header;
+        uint8_t             data[8];
+        if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO1, &header, data) != HAL_OK)
+        {
+            Error_Handler();
+            return;
+        }
+        const CAN_CallbackMap* map = get_map(hcan);
+        if (map != nullptr)
+            for (size_t i = 0; i < map->callback_count; i++)
+                map->callbacks[i](hcan, &header, data);
     }
-    const CAN_CallbackMap* map = get_map(hcan);
-    if (map != nullptr)
-        for (size_t i = 0; i < map->callback_count; i++)
-            map->callbacks[i](hcan, &header, data);
 }
 
 void CAN_TxMailboxCpltCallback(CAN_HandleTypeDef* hcan)
